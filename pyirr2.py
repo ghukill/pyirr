8f9c0a240eb480db68fab850606fdcb7f137a0ff
Graham Hukill <ghukill@gmail.com>
2021-04-29 22:22:10 +0200

    rename

--- pyirr2.py
+++ pyirr2.py
@@ -7,11 +7,16 @@ Resources
 """
 
 import argparse
+import logging
 import uuid
 
 from nltk import agreement
 import numpy as np
 import pandas as pd
+
+from pyirr import read_data, intraclass_correlation
+
+logging.getLogger().setLevel(logging.DEBUG)
 
 
 def main(input_filepath, sheet_name, rater_cols, skiprows):
@@ -25,7 +30,6 @@ def main(input_filepath, sheet_name, rater_cols, skiprows):
 
     # read excel document into pandas dataframe
     df = pd.read_excel(input_filepath, sheet_name=sheet_name, skiprows=int(skiprows))
-    print(df)
 
     # drop row where Code=='Totals'
     df = df[df.Code != "Totals"]
@@ -35,18 +39,17 @@ def main(input_filepath, sheet_name, rater_cols, skiprows):
     for col in rater_cols:
         r = df[col]
         r = r[~np.isnan(r)]
-        rater_scores[f"{col}|{str(uuid.uuid4())}"] = r
+        rater_scores[col] = r
 
     # integrity check
     print("integrity check for scores:")
-    int_test = {k:len(v) for k,v in rater_scores.items()}
+    int_test = {k: len(v) for k, v in rater_scores.items()}
     print(int_test)
 
     # structure data
     nltk_structured_scores = []
     for k, v in rater_scores.items():
-        nltk_structured_scores.extend([(k, i, s) for i, s in enumerate(v)])
-    print(nltk_structured_scores)
+        nltk_structured_scores.extend([(k, str(i), int(s)) for i, s in enumerate(v)])
 
     # run NLTK agreement
     agreements = agreement.AnnotationTask(data=nltk_structured_scores)
@@ -80,7 +83,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--input", default="input.xlsx")
     parser.add_argument("--sheet_name", default="my_data")
-    parser.add_argument("--cols", nargs="+", default=["R1", "R2"])
+    parser.add_argument("--cols", nargs="+", default=["RF", "LR"])
     parser.add_argument("--skiprows", default=2)
 
     args = parser.parse_args()
