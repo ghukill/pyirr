a0c54bc7190235f43fe60bc9cdd1e08572454ed4
Graham Hukill <ghukill@gmail.com>
2021-04-29 19:30:40 +0200

    parameterize the cols

--- pyirr.py
+++ pyirr.py
@@ -7,13 +7,14 @@ Resources
 """
 
 import argparse
+import uuid
 
 from nltk import agreement
 import numpy as np
 import pandas as pd
 
 
-def main(input_filepath, rater_cols=["RF", "LR"], score_low=0, score_high=10):
+def main(input_filepath, rater_cols, score_low=0, score_high=10):
 
     """
 
@@ -31,9 +32,9 @@ def main(input_filepath, rater_cols=["RF", "LR"], score_low=0, score_high=10):
         r = df[col]
         r = r[~np.isnan(r)]
         r = [x for x in r if score_low <= x <= score_high]
-        rater_scores[col] = r
+        rater_scores[f"{col}|{str(uuid.uuid4())}"] = r
 
-    # TODO: intergrity check that number of scores equal
+    # TODO: integrity check that number of scores equal
 
     # structure data
     nltk_structured_scores = []
@@ -53,7 +54,7 @@ def main(input_filepath, rater_cols=["RF", "LR"], score_low=0, score_high=10):
     output_df = pd.DataFrame.from_dict(scores, orient="index")
 
     # print
-    print()
+    print(output_df.to_markdown())
 
 
 if __name__ == "__main__":
@@ -71,9 +72,10 @@ if __name__ == "__main__":
     # parse args
     parser = argparse.ArgumentParser()
     parser.add_argument("--input", default="input.xlsx")
+    parser.add_argument("--cols", nargs="+", default=["RF", "LR"])
 
     # TODO: add args: skip rows, column identifiers
 
     args = parser.parse_args()
 
-    main(args.input)
+    main(args.input, args.cols)
